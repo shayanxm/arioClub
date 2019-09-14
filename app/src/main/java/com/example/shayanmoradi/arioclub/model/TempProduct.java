63ddb2385f129806413f0f3840d0e04e8e44f2d9
shayanxm <shayan.mwp@gmail.com>
2019-09-14 16:29:24 +0430

    icon added name on top shows-needs text too creating factor full created samlple product nad offer added
    UI needs work- a little better

---
+++ app/src/main/java/com/example/shayanmoradi/arioclub/model/TempProduct.java
@@ -0,0 +1,27 @@
+package com.example.shayanmoradi.arioclub.model;
+
+public class TempProduct {
+    private Product product;
+    private int count;
+
+    public TempProduct(Product product, int count) {
+        this.product = product;
+        this.count = count;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
